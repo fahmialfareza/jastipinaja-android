a74f73b0f2ff5233b132893e0c7f41a8c531ca2d
Dino Keylas <dinokeylas@gmail.com>
2020-08-21 16:20:04 +0700

    create User model class

---
+++ app/src/main/java/com/dinokeylas/jastipinaja/model/User.kt
@@ -0,0 +1,11 @@
+package com.dinokeylas.jastipinaja.model
+
+data class User (
+    val userName: String = "",
+    val fullName: String = "",
+    val email: String = "",
+    val address: String = "",
+    val phoneNumber: String = "",
+    val profileImageUrl: String = "",
+    val password: String = ""
+)
