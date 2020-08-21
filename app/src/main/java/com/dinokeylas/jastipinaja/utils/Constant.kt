822a7bee8d67f420807456aa7cdc87aa3bc4d185
Dino Keylas <dinokeylas@gmail.com>
2020-08-21 16:20:22 +0700

    create utils

---
+++ app/src/main/java/com/dinokeylas/jastipinaja/utils/Constant.kt
@@ -0,0 +1,14 @@
+package com.dinokeylas.jastipinaja.utils
+
+interface Constant {
+
+    interface Collections{
+        companion object{
+            val USER = "user"
+            val TRANSACTION = "transaction"
+            val ITEM = "item"
+            val BANNER_IMAGES = "bannerImages"
+        }
+    }
+
+}
